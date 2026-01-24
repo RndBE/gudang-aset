27a7885267309d91a5e5165ca1ca7b8a3abe6cd6
RndBE <rnd.software.be@gmail.com>
2026-01-24 10:35:52 +0700

    shandy-

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AWASS - Login</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="icon" type="image/svg+xml" href="{{ Vite::asset('resources/icon/head-icon.png') }}">
+    {{-- <link rel="icon" type="image/svg+xml" href="{{ Vite::asset('resources/icon/head-icon.png') }}"> --}}
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Sonsie+One&display=swap" rel="stylesheet">
